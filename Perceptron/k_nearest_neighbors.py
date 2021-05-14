9d484538fbba02d78dde10ffc911d3ed5a6413a1
Philip-Crowther <crowtherpb@gmail.com>
2021-05-13 22:53:29 -0400

    created file and brief outline for an implementation of the k-nearest neighbors algorithm

---
+++ Perceptron/k_nearest_neighbors.py
@@ -0,0 +1,17 @@
+# k_nearest_neighbors.py - implementation of the k-nearest neighbors algorithm
+
+class KNeighbors:
+    def __init__(self):
+        pass
+
+    def find_k_nearest_neighbors(self):
+        pass
+
+
+def main():
+    """main function to hold testing"""
+    pass
+
+
+if __name__ == '__main__':
+    main()
--- Perceptron/perceptron.py
+++ Perceptron/perceptron.py
@@ -87,6 +87,8 @@ def main():
     accuracy_test_data = [[r.randint(-50, 50), r.randint(-50, 50)] for _ in range(100)]
     accuracy_test_data = [[1] + datum if datum[0] + bias > datum[1] else [-1] + datum for datum in accuracy_test_data]
 
+    # 3D
+
     p = Perceptron()
     p.train(data)
     print('accuracy with original dataset: ', p.test_accuracy(data))
