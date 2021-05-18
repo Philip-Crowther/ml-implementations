e7a65045b0d381f1484107614c9a70289a44df1c
Philip-Crowther <crowtherpb@gmail.com>
2021-05-18 15:59:23 -0400

    laid out base elements of the bayes file

---
+++ NaiveBayesClassifier/naive_bayes_classifier.py
@@ -0,0 +1,13 @@
+# naive_bayes_classifier.py - implementation of the naive bayes classification algorithm
+
+class Bayes:
+    def __init__(self):
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
