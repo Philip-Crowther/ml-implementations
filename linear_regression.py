56f65a3ecc5ff7ba4df9466a3a2cd0abf8767f0d
Philip Crowther <crowtherpb@gmail.com>
2021-09-07 15:48:12 -0400

    create linear_regression.py
    create bare bones outline to turn into a trainable linear_regresion model class

---
+++ linear_regression.py
@@ -0,0 +1,21 @@
+# linear_regression.py - implementation of linear regression
+
+
+class LinearRegression:
+
+    def __init__(self):
+        pass
+
+    def train(self, x_train, y_train):
+        pass
+
+    def predict(self, x):
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
